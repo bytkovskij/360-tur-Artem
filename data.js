c3bb0a9f37eb5a46067c29efce8f891631f2188c
Olexandr_B <91286050+bytkovskij@users.noreply.github.com>
2023-11-10 17:59:02 +0200

    Commit

---
+++ data.js
@@ -0,0 +1,721 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-",
+      "name": "Загальна",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.0024161160967857143,
+        "pitch": 0.3789139075403014,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.7984664216601125,
+          "pitch": 0.47372924630278135,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -0.7877000011979405,
+          "pitch": 0.4710039370393275,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": 0.24528012483035688,
+          "pitch": 0.24944716942877676,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-",
+      "name": "Навіс",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.103468978590035,
+          "pitch": -0.42666899498545163,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -1.1453503534967577,
+          "pitch": 0.00804188868272604,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": 0.08959489352118233,
+          "pitch": -0.024322085861149034,
+          "rotation": 0,
+          "target": "2-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-",
+      "name": "Тераса",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.00805536577843391,
+        "pitch": 1.7763568394002505e-15,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.908580059400883,
+          "pitch": -0.6045592123828154,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.646274361465675,
+          "pitch": -0.01130628829875846,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": -2.681495864964452,
+          "pitch": -0.005912896815541302,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": 0.1921295907023115,
+          "pitch": 0.0814969684651814,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -0.24375942630980418,
+          "pitch": 0.03231273481832275,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.8063008017487565,
+          "pitch": 0.03188904765749001,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -1.4369697759807138,
+          "pitch": 0.04132864996253183,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": -1.7477890554488376,
+          "pitch": 0.22663547490606106,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": -2.0488626685086846,
+          "pitch": 0.08084704566660861,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3--",
+      "name": "Лаундж зона",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.38128731351261713,
+        "pitch": -0.03730708650064507,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.3242962368131952,
+          "pitch": -0.5297075768504929,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.04974991560598596,
+          "pitch": 0.16279259754494646,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.3311167051959103,
+          "pitch": 0.011433325551813311,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.842674971984616,
+          "pitch": -0.05829673848072403,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 0.6890925711313507,
+          "pitch": 0.1348633605727514,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 2.088848306566641,
+          "pitch": 0.02636105115896825,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 1.5445228853279946,
+          "pitch": 0.04726808753808065,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.9048150736474083,
+          "pitch": 0.08036352608186625,
+          "rotation": 0,
+          "target": "4-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-",
+      "name": "Галявина",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.1637924374948625,
+        "pitch": -0.018651164656198205,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.20577976371811602,
+          "pitch": -0.4705456115322466,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.16718862473273255,
+          "pitch": 0.013136933936355177,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.4271852433490686,
+          "pitch": 0.07061834103680908,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 0.4580067463547479,
+          "pitch": -0.009958249685665166,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 0.19555263514360632,
+          "pitch": 0.017831755310927733,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 1.1434054771161701,
+          "pitch": 0.04639368800406629,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.7554510421146929,
+          "pitch": 0.03338830417273897,
+          "rotation": 0,
+          "target": "6-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-",
+      "name": "Баня",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.4537856055185312,
+        "pitch": 0.009326771625158159,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.17996381711764542,
+          "pitch": -0.36746256368278907,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.6893067636504284,
+          "pitch": -0.005172119416094745,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.2357707306365082,
+          "pitch": 0.014652151662680524,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.0356376218003938,
+          "pitch": 0.20018718033016114,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 0.2868355290719631,
+          "pitch": 0.2163443263805842,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.09911287008602443,
+          "pitch": 0.04273214910449674,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": -0.2271774235832975,
+          "pitch": 0.0763338203057522,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "6-",
+      "name": "Город",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.2081006083578938,
+        "pitch": 0.024249606225422937,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.7125423542379643,
+          "pitch": 0.07189104535759938,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": 0.20189762071119333,
+          "pitch": -0.3554140973415052,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": -0.8079408975823767,
+          "pitch": 0.046946471286823055,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -1.418305074589405,
+          "pitch": 0.04162333486608638,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -2.1176995155938876,
+          "pitch": 0.04988542787916472,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -0.1754253681929363,
+          "pitch": 0.12576428896129244,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 0.5958022826651934,
+          "pitch": 0.09231024817422195,
+          "rotation": 0,
+          "target": "8-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "7-",
+      "name": "Прохід",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.029536341187592186,
+        "pitch": 0.04476850380077124,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.6792648516777415,
+          "pitch": -0.4355246046516488,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 0.018708490833759583,
+          "pitch": 0.0632653932481233,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": -0.6318499192231819,
+          "pitch": 0.04681541010207191,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": -1.0749789348642587,
+          "pitch": 0.01658371225811983,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": -1.6505879525463758,
+          "pitch": -0.007403879689238835,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -2.123087373006216,
+          "pitch": 0.13698718280709699,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 2.558163834130969,
+          "pitch": 0.019117592180389664,
+          "rotation": 0,
+          "target": "8-"
+        },
+        {
+          "yaw": 1.9401528705009667,
+          "pitch": 0.004242753405389266,
+          "rotation": 0,
+          "target": "9-"
+        },
+        {
+          "yaw": 0.9604577752071819,
+          "pitch": -0.08876385684919619,
+          "rotation": 0,
+          "target": "1-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-",
+      "name": "Бар",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 2.7554123525182153,
+        "pitch": 0.005222144221754377,
+        "fov": 1.3635740115987254
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.35316219613332933,
+          "pitch": -0.03748601533952112,
+          "rotation": 0,
+          "target": "5-"
+        },
+        {
+          "yaw": -0.2541879040670203,
+          "pitch": 0.149325947113919,
+          "rotation": 0,
+          "target": "6-"
+        },
+        {
+          "yaw": 0.2100317238938878,
+          "pitch": -0.017372484451765757,
+          "rotation": 0,
+          "target": "4-"
+        },
+        {
+          "yaw": 1.0001869754379893,
+          "pitch": 0.2039399364884389,
+          "rotation": 0,
+          "target": "7-"
+        },
+        {
+          "yaw": 0.8120056710887766,
+          "pitch": 0.012998419154415686,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.4722712526524404,
+          "pitch": 0.04817688038361112,
+          "rotation": 0,
+          "target": "3--"
+        },
+        {
+          "yaw": 2.2704036073471805,
+          "pitch": -0.5206600929272973,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 2.9402125401918475,
+          "pitch": 0.051380174832122094,
+          "rotation": 0,
+          "target": "9-"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "9-",
+      "name": "Гараж",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.6443470191933134,
+          "pitch": -0.5387360150596425,
+          "rotation": 0,
+          "target": "0-"
+        },
+        {
+          "yaw": 1.6915804403659607,
+          "pitch": 0.0010761493372299924,
+          "rotation": 0,
+          "target": "1-"
+        },
+        {
+          "yaw": 0.4375612403735172,
+          "pitch": -0.1026248019224063,
+          "rotation": 0,
+          "target": "2-"
+        },
+        {
+          "yaw": 0.18110700163719073,
+          "pitch": -0.06810482388943662,
+          "rotation": 0,
+          "target": "7-"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "360 tur Artem",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
